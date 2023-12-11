7276a05c7adad9659c2d51594913969c6d2014c5
catonrug <aigars.kadikis@gmail.com>
2023-12-11 14:27:35 +0200

    next

--- usr/local/bin/zabbix_partitioning.sh
+++ usr/local/bin/zabbix_partitioning.sh
@@ -4,10 +4,10 @@
 DATABASE=zabbix
 
 # SQL command to maintain partitions
-SQLCOMMAND="CALL partition_maintenance_all(\"$DATABASE\");"
+COMMAND="CALL partition_maintenance_all(\"$DATABASE\");"
 
 # Drop old and create new partitions
 mysql \
 --defaults-file=/etc/zabbix/zabbix_partitioning.cnf \
 --database="$DATABASE" \
---execute="$SQLCOMMAND"
+--execute="$COMMAND"
--- usr/share/zabbix-sql-scripts/mysql/zabbix_partitioning/create_user.sql
+++ usr/share/zabbix-sql-scripts/mysql/zabbix_partitioning/create_user.sql
@@ -1,3 +1,9 @@
+SET GLOBAL validate_password.LENGTH = 4;
+SET GLOBAL validate_password.policy = 0;
+SET GLOBAL validate_password.mixed_case_count = 0;
+SET GLOBAL validate_password.number_count = 0;
+SET GLOBAL validate_password.special_char_count = 0;
+SET GLOBAL validate_password.check_user_name = 0;
 DROP USER IF EXISTS 'zbx_part'@'127.0.0.1';
 CREATE USER 'zbx_part'@'127.0.0.1' IDENTIFIED WITH mysql_native_password BY 'ZHYImQ65nXJ8z9knwRXlIj0x';
 GRANT SELECT, ALTER, DROP ON zabbix.history TO 'zbx_part'@'127.0.0.1';
